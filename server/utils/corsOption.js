ad83ece67a895e52914b00f2ba0623bd3bd4588c
kennyoyaf <kehindeoyafemi@gmail.com>
2025-07-09 19:36:25 +0100

    Update CORS middleware to allow production domain

--- server/utils/corsOption.js
+++ server/utils/corsOption.js
@@ -1,18 +1,19 @@
-// options for cors
+// utils/corsOption.js
 module.exports = (req, res, next) => {
   const allowedOrigins = [
     "http://localhost:3000",
-    "https://amazona-ecommerce-rinq.vercel.app/",
-    "https://amazona-ecommerce-o1bx.vercel.app/",
-    "https://amazonaecommerce.com",
-    "https://www.amazonaecommerce.com",
+    "https://amazona-ecommerce.vercel.app",
+    "https://amazona-ecommerce-o1bx.vercel.app",
+    "https://amazona-ecommerce-rinq.vercel.app",
   ];
   const origin = req.headers.origin;
+
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true"); // Add this line
+    res.setHeader("Access-Control-Allow-Credentials", "true");
   }
+
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "content-type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 };

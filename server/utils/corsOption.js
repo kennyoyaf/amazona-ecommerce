57caf6778b881151033d0d07e255213b5c73482f
kennyoyaf <kehindeoyafemi@gmail.com>
2025-07-09 17:46:49 +0100

    Update CORS to allow Vercel frontend

--- server/utils/corsOption.js
+++ server/utils/corsOption.js
@@ -1,12 +1,15 @@
 // options for cors
 module.exports = (req, res, next) => {
-  const allowedOrigins = ['http://localhost:3000'];
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://amazona-ecommerce-rinq.vercel.app/",
+  ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Add this line
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true"); // Add this line
   }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'content-type, Authorization');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "content-type, Authorization");
   next();
 };

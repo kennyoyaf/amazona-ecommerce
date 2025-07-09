f040adcc0049c17117d71e2cafb3c7529168ce0d
kennyoyaf <kehindeoyafemi@gmail.com>
2025-07-09 18:09:47 +0100

    Update CORS to allow new frontend domain

--- server/utils/corsOption.js
+++ server/utils/corsOption.js
@@ -3,6 +3,9 @@ module.exports = (req, res, next) => {
   const allowedOrigins = [
     "http://localhost:3000",
     "https://amazona-ecommerce-rinq.vercel.app/",
+    "https://amazona-ecommerce-o1bx.vercel.app/",
+    "https://amazonaecommerce.com",
+    "https://www.amazonaecommerce.com",
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
